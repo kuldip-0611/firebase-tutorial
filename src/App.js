6ed9b69331e35e4c0d52a6f0d30f8f0e9ca53407
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-05 17:10:57 +0530

    write data in fireStore

--- src/App.js
+++ src/App.js
@@ -1,46 +1,62 @@
 
 import './App.css';
+import {getFirestore,collection,addDoc} from 'firebase/firestore'
 // import { useFirebase } from './context/Firebase';
 // import { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword ,onAuthStateChanged,signOut} from 'firebase/auth';
+// import { getAuth, createUserWithEmailAndPassword ,onAuthStateChanged,signOut} from 'firebase/auth';
 import { app } from './firebase';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import { useEffect,useState } from 'react';
+// import Signup from './pages/Signup';
+// import Signin from './pages/Signin';
+// import { useEffect,useState } from 'react';
 
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
 
+const fireStore = getFirestore(app);
 
+const writeData =async  ()=>{
+  const result = await addDoc(collection(fireStore,'cities'),{
+    name:'bhavnagar',
+    state:'gujarat',
+    country:'india'
+  })
+  console.log(result)
+}
+const addAnotherData = async ()=>{
+  const result = await addDoc(collection(fireStore,'cities/BdGS66ACAC0KsOc2VDiD/food'),{
+    food:'gathiya'
+  })
+}
 
 
 function App() {
-  const [user,setUser] = useState(null);
+  // const [user,setUser] = useState(null);
+
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
   // const firebase = useFirebase()
-  useEffect(()=>{
-    onAuthStateChanged(auth,(user)=>{
-      if(user){
-        setUser(user)
+  // useEffect(()=>{
+  //   onAuthStateChanged(auth,(user)=>{
+  //     if(user){
+  //       setUser(user)
 
-      }
-      else{
-        setUser(null)
-      }
-    })
-  })
+  //     }
+  //     else{
+  //       setUser(null)
+  //     }
+  //   })
+  // })
 
   
   
 
-  const signup = () => {
-    createUserWithEmailAndPassword(
-      auth,
-      "pandyakuldip2001@gmail.com",
-      "Kuldip@0611")
-      .then(value => console.log(value));
-  }
+  // const signup = () => {
+  //   createUserWithEmailAndPassword(
+  //     auth,
+  //     "pandyakuldip2001@gmail.com",
+  //     "Kuldip@0611")
+  //     .then(value => console.log(value));
+  // }
 
   // if(user === null){
   //  <>
@@ -51,20 +67,22 @@ function App() {
 
   return (
     <>
-    {user ? <h1> {user.email}  <button onClick={()=>signOut(auth)}>Logout</button> </h1> : <Signin />}
+      <button onClick={writeData}>Add Data To fireStore</button>
+      <button onClick={addAnotherData} > add another collection </button> 
+    {/* {user ? <h1> {user.email}  <button onClick={()=>signOut(auth)}>Logout</button> </h1> : <Signin />}
     <div className="App">
       <div>
         Fire Base Basics
       </div>
       <div className="btn">
         <button onClick={signup} >Put Data</button>
-      </div>
+      </div> */}
 
       {/* <h1>{user.email}</h1> */}
       
     
 
-    </div>
+    {/* </div> */}
     
     {/* <lable>Name</lable>
     <input type="email" value={email} onChange={e=>setEmail(e.target.value)}/>
