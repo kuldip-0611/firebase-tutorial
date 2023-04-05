57876797c782fafb6e9035147e54b2b604f18fcd
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-05 15:17:49 +0530

    firebase using useContext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+// import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 import './App.css';
-import { app } from './firebase';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
+import { useFirebase } from './context/Firebase';
+import { useState } from 'react';
+// import { app } from './firebase';
+// import Signup from './pages/Signup';
+// import Signin from './pages/Signin';
 
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const firebase = useFirebase()
+  
 
-  const signup = () => {
-    createUserWithEmailAndPassword(
-      auth,
-      "pandyakuldip2001@gmail.com",
-      "Kuldip@0611")
-      .then(value => console.log(value));
-  }
+  // const signup = () => {
+  //   createUserWithEmailAndPassword(
+  //     auth,
+  //     "pandyakuldip2001@gmail.com",
+  //     "Kuldip@0611")
+  //     .then(value => console.log(value));
+  // }
 
 
 
   return (
-    <div className="App">
-      <div>
-        Fire Base Basics
-      </div>
-      <div className="btn">
-        <button onClick={signup} >Put Data</button>
-      </div>
-      <Signup />
-      <Signin />
+    // <div className="App">
+    //   <div>
+    //     Fire Base Basics
+    //   </div>
+    //   <div className="btn">
+    //     <button onClick={signup} >Put Data</button>
+    //   </div>
+    //   <Signup />
+    //   <Signin />
 
-    </div>
+    // </div>
+    <>
+    <lable>Name</lable>
+    <input type="email" value={email} onChange={e=>setEmail(e.target.value)}/>
+    <lable>Password</lable>
+    <input type="password" value={password} onChange={e=>setPassword(e.target.value)}  />
+    <button onClick={()=>{
+       firebase.signUp(email,password) ;
+       firebase.putData("users/",email)}}>Submit</button>
+    </>
+   
   );
 }
 
---
+++ src/context/Firebase.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext } from "react";
+import {initializeApp} from 'firebase/app'
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {getDatabase, ref, set} from 'firebase/database'
+const FirebaseContext = createContext(null);
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBoKb-WEY3TKsXQsYBcs8lX8xE-canuBfc",
+    authDomain: "first-project-7e25b.firebaseapp.com",
+    projectId: "first-project-7e25b",
+    storageBucket: "first-project-7e25b.appspot.com",
+    messagingSenderId: "965822896527",
+    appId: "1:965822896527:web:be4e8778316619ccc793fa",
+    databaseURL: "https://first-project-7e25b-default-rtdb.firebaseio.com"
+};
+const FirebaseApp = initializeApp(firebaseConfig)
+const firebaseAuth = getAuth(FirebaseApp);
+const database = getDatabase(FirebaseApp);
+
+export const useFirebase = ()=> useContext(FirebaseContext);
+export const FirebaseProvider = (props) => {
+    const signUp = (email,password)=>{
+        return createUserWithEmailAndPassword(firebaseAuth,email,password);
+
+    }
+    const putData = (key,data)=> set(ref(database,key),data)
+
+
+    
+  return (
+  <FirebaseContext.Provider value={{signUp,putData}}>
+    {props.children}
+  </FirebaseContext.Provider>)
+};
